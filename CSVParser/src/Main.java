e809a6d93a5622f569a19f3a77062e31fbc866f9
Alex Oladele <dragid10@gmail.com>
2016-10-06 13:54:54 -0400

    Made the program a tiny bit more efficient. Prolly neglible though

--- CSVParser/src/Main.java
+++ CSVParser/src/Main.java
@@ -9,13 +9,12 @@ public class Main {
     static void parse() throws FileNotFoundException {
         ArrayList<String> counties = new ArrayList<>();
         ArrayList<String> votingNumbers = new ArrayList<>();
-        PrintWriter countyFile = new PrintWriter("counties.txt");
-        PrintWriter numFile = new PrintWriter("votingNumbers.txt");
+        PrintWriter file = new PrintWriter("counties.txt");
         try {
             BufferedReader inFile = new BufferedReader(new FileReader("RegisteredOhioVoters.csv"));
-            String line;
-            String county;
-            String voterNum;
+            String line = "";
+            String county = "";
+            String voterNum = "";
             while ((line = inFile.readLine()) != null) {
                 county = line.substring(0, line.indexOf(','));
                 voterNum = line.substring(line.indexOf(',') + 1, line.length());
@@ -24,24 +23,19 @@ public class Main {
             }
             inFile.close();
 
-
             for (String s : counties) {
-                countyFile.println(s);
-//                System.out.println(s);
+                file.println(s);
+                System.out.println(s);
             }
+            file = new PrintWriter("votingNumbers.txt");
             for (String s : votingNumbers) {
-                numFile.println(s);
-
-//                System.out.println(s);
+                file.println(s);
+                System.out.println(s);
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (countyFile != null && numFile != null)
-                countyFile.close();
-                numFile.close();
+            file.close();
         }
     }
 }
