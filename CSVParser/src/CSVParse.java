dd4998f71e9f8766b58217e68f91f090879529b6
Alex Oladele <dragid10@gmail.com>
2016-10-19 15:40:37 -0400

    Finalize Error Handling and Parsing

--- CSVParser/src/CSVParse.java
+++ CSVParser/src/CSVParse.java
@@ -8,7 +8,15 @@ import java.util.Date;
 * Will still have to tweak even more to ge the individual voting nums (democratic, republican, green, etc..
 */
 class CSVParse {
+    private int lineNumber = 0;
+    private File readFile;
+    private PrintWriter err;
+
     void parse() throws FileNotFoundException {
+        Date date = new Date();
+        Format formatter = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
+        err = new PrintWriter("logs/" + formatter.format(date) + ".txt");
+
         // Houses the counties from the read in line
         ArrayList<String> counties = new ArrayList<>();
 
@@ -20,11 +28,13 @@ class CSVParse {
              PrintWriter votNumWriter = new PrintWriter("OutputFiles/votingNumbers.txt")) {
             File dir = new File("VoterRegFiles/");
             // Read in lines will be assigned to this variable
-            String line, county = null, voterNum;
+            String line, county, voterNum;
 
             // Reads all .csv files in a directory one at a time and proceeds to do things with them
             // We will have to tweak this to work with JFileChooser
             for (File file : dir.listFiles()) {
+                readFile = file;
+                lineNumber = 0;
 
                 votNumWriter.flush();
                 countWriter.flush();
@@ -34,10 +44,11 @@ class CSVParse {
 
                 // The while loop that reads in each line from the .csv file
                 while ((line = inFile.readLine()) != null) {
+                    lineNumber++;
 
                     if (line.indexOf(',') == -1) {
-                        // Throw new exception if the line is corrupt
-                        throw new IllegalArgumentException();
+                        // Prints to log file if the line is corrupt
+                        printIllegalArgToLogFile();
                     } else {
                         // Isolates county and voting nums in each line, and adds them to their respective arraylists
                         county = line.substring(0, line.indexOf(','));
@@ -47,64 +58,51 @@ class CSVParse {
                     }
                 }
 
-//                inFile.close();
-
                 /*
                 * Goes through each arraylist and prints the indicies to a text file
                 * The PrintWriter must first be closed before it can write to the votingNums
                 * If not closed, it will NOT write.
                 */
                 for (String s : counties) {
-//                    pFile.println(s);
                     countWriter.append(s).append('\n');
 
                     // Used to check if the counties are correct
 //                    System.out.println(s);
                 }
-
                 for (String s : votingNumbers) {
-//                    pFile.println(s);
                     votNumWriter.append(s).append('\n');
                     // Used to check if the voting nums are correct
 //                    System.out.println(s);
                 }
-
                 counties.clear();
                 votingNumbers.clear();
-
             }
-
-            // TODO work on error handling better! (get line # where dat is corrupt)
-            // TODO get program to continue when error occurs!
         } catch (IOException e) {
-           /* Date date = new Date();
-            Format formatter = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
-            PrintWriter err = new PrintWriter("logs/" + formatter.format(date) + ".txt");
-            err.append("The File could not be opened for writing, or wrong type of file provided!\n");
-            err.append("-----------------------------------------------");
-            e.printStackTrace(err);*/
-            e.printStackTrace();
-
+            printIOExcepToLogFile(e);
         } catch (IllegalArgumentException p) {
-            Throwable throwable = p;
-            Date date = new Date();
-            Format formatter = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
-            PrintWriter err = new PrintWriter("logs/" + formatter.format(date) + ".txt");
-            err.append("The Line could not be read, or is corrupt! at line " + getLineNumber() + '\n');
-            err.append("------------------------------------------------------\n\r\n\r");
-            err.append("StackTrace: \n");
-            p.printStackTrace(err);
-//            err.append(p.getMessage());
-//            err.append(p.getLocalizedMessage());
-            err.flush();
-            err.close();
-
+            p.printStackTrace();
         }
 
     }
 
-    public static int getLineNumber() {
-        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    private void printIOExcepToLogFile(Exception e) throws FileNotFoundException {
+        Date date = new Date();
+        Format formatter = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
+        PrintWriter err = new PrintWriter("logs/" + formatter.format(date) + ".txt");
+        err.append("An IO Exception of some sort has occurred \n");
+        err.append("------------------------------------------------------\n\r");
+        err.append("StackTrace: \n");
+        e.printStackTrace(err);
+        err.flush();
     }
+
+    private void printIllegalArgToLogFile() throws FileNotFoundException {
+        String fileName = "" + readFile;
+        fileName = fileName.substring(fileName.indexOf('/') + 1, fileName.length());
+        err.append("The Line(" + lineNumber + ") in " + "" + fileName + " could not be read, or is corrupt!" + "\n");
+        err.append("------------------------------------------------------\n\r");
+        err.flush();
+    }
+
 }
 
