6d3f9ffbd2939be8dbf38697ecc5b775a0c2c21c
Alex Oladele <dragid10@gmail.com>
2016-10-19 15:43:59 -0400

    Implement error handling for Files suddenly not being there

--- CSVParser/src/Main.java
+++ CSVParser/src/Main.java
@@ -1,12 +1,23 @@
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         CSVParse csvParse = new CSVParse();
         try {
             csvParse.parse();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Date date = new Date();
+            Format formatter = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
+            PrintWriter err = new PrintWriter("logs/" + formatter.format(date) + ".txt");
+            err.append("The file is not found!!\n");
+            err.append("------------------------------------------------------\n\r");
+            err.append("StackTrace: \n");
+            e.printStackTrace(err);
+            err.flush();
         }
     }
 }
