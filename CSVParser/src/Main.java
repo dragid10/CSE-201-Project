c976f60b0df7151c28031274b6a68621e9d6b448
Alex Oladele <dragid10@gmail.com>
2016-11-28 14:20:03 -0500

    Update parse to include directory from user

--- CSVParser/src/Main.java
+++ CSVParser/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         CSVParse csvParse = CSVParse.getInstance();
         try {
-            csvParse.parse();
+            csvParse.parse("VoterRegFiles/");
             csvParse.loadVoterData();
         } catch (FileNotFoundException e) {
             Date date = new Date();
