67bec1462a0a8dbd69ce5f6bcc16a09f38d22f10
Alex Oladele <dragid10@gmail.com>
2016-11-29 00:40:30 -0500

    Move MultiMap to this class
    It just seemed like a betterish idea to be here for some reason

--- CSVParser/src/VotingData/VoterData.java
+++ CSVParser/src/VotingData/VoterData.java
@@ -1,14 +1,27 @@
 package VotingData;
 
+// Import the MultiMap
+// Docs here: https://google.github.io/guava/releases/19.0/api/docs/com/google/common/collect/Multimap.html
+
+import com.google.common.collect.HashMultimap;
+
+import java.util.logging.Logger;
 
 public class VoterData {
-    private String precinct;
+    //=============================================================== Variables
+    private static final Logger LOGGER = Logger.getLogger( VoterData.class.getName() );
+
+    public String precinct;
     private int demVotes;
     private int repVotes;
     private int othVotes;
 
+    // HashMap that houses all of the CountyData
+    public static HashMultimap<String, VoterData> countyVoteInfo = HashMultimap.create();
 
-    public VoterData(int demVotes, int repVotes, int othVotes) {
+    //=============================================================== Constructors
+    public VoterData(String precinct, int demVotes, int repVotes, int othVotes) {
+        setPrecinct(precinct);
         setDemVotes(demVotes);
         setRepVotes(repVotes);
         setOthVotes(othVotes);
@@ -20,6 +33,16 @@ public class VoterData {
         setRepVotes(0);
         setOthVotes(0);
     }
+
+    public VoterData(String precinct) {
+        setPrecinct(precinct);
+        setDemVotes(0);
+        setRepVotes(0);
+        setOthVotes(0);
+    }
+
+
+    //=============================================================== Getters / Setters
 
     private void setPrecinct(String precinct) {
         this.precinct = precinct;
